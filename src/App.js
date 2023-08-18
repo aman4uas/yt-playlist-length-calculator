13f055fa22a25bebfe254e640282e77816826d67
aman4uas <aman4u.as@gmail.com>
2023-08-18 10:06:43 +0530

    update navbar

--- src/App.js
+++ src/App.js
@@ -17,13 +17,13 @@ function App() {
 
   return (
     <div className="h-screen flex flex-col justify-between overflow-x-hidden">
-      <nav className='h-12 md:h-14 bg-black flex justify-between items-center relative' >
-        <div className="p-1 pl-2 h-full flex items-center gap-4 hover:cursor-pointer" onClick={navigateToHome}>
+      <nav className='h-12 md:h-16 bg-black flex justify-between items-center relative' >
+        <div className="md:ml-[7vw] p-1 pl-2 h-full flex items-center gap-4 hover:cursor-pointer" onClick={navigateToHome}>
           <img className="h-full" src="logo-removebg.png" alt="logo-img" />
           <h1 className="text-3xl pl-2 font-bold">YT Tool</h1>
         </div>
 
-        <div className="hidden md:flex flex-row justify-between items-center m-2">
+        <div className="mr-[7vw] hidden md:flex flex-row justify-between items-center m-2">
           <NavLink className={({ isActive }) => (isActive ? 'border rounded-md p-1 font-semibold' : '')} to="/"><div className="px-2 mx-3 text-white hover:font-semibold">Home</div></NavLink>
           <NavLink className={({ isActive }) => (isActive ? 'border rounded-md p-1 font-semibold' : '')} to="/about"><div className="px-2 mx-3 text-white hover:font-semibold">About</div></NavLink>
           <NavLink className={({ isActive }) => (isActive ? 'border rounded-md p-1 font-semibold' : '')} to="/contact"><div className="px-2 mx-3 text-white hover:font-semibold">Contact</div></NavLink>
